e877ec1a9634883f75ba37007d515e2baee75762
will.109 <will.109@kakaopaycorp.com>
2022-02-18 00:37:14 +0900

    코드 정리

--- src/main/scala/RepositoryRank.scala
+++ src/main/scala/RepositoryRank.scala
@@ -2,25 +2,22 @@ package io.icednut.study
 
 import cats.Applicative
 import cats.effect.{IO, IOApp}
-import cats.implicits._
-import org.kohsuke.github.GHIssueState
+import org.kohsuke.github.{GHIssueState, GitHub}
 
-class RepositoryRank[F[_] : Applicative : FromGithub] {
+class RepositoryRank[F[_] : Applicative] {
 
-  def getPoint(repositoryName: String): F[Int] =
-    for {
-      github <- FromGithub[F].connect
-    } yield {
-      val repository = github.getRepository(repositoryName)
+  def getPoint(repositoryName: String): F[Int] = Applicative[F].pure {
+    val github = GitHub.connect()
+    val repository = github.getRepository(repositoryName)
 
-      val issuePoint = if (repository.hasIssues) 1 else 0
-      val readmePoint = if (repository.getReadme() != null) 1 else 0
-      val prPoint = if (repository.getPullRequests(GHIssueState.CLOSED).size > 0) 1 else 0
-      val starPoint = repository.getStargazersCount
-      val forkPoint = repository.getForksCount
+    val issuePoint = if (repository.hasIssues) 1 else 0
+    val readmePoint = if (repository.getReadme() != null) 1 else 0
+    val prPoint = if (repository.getPullRequests(GHIssueState.CLOSED).size > 0) 1 else 0
+    val starPoint = repository.getStargazersCount
+    val forkPoint = repository.getForksCount
 
-      issuePoint + readmePoint + prPoint + starPoint + forkPoint
-    }
+    issuePoint + readmePoint + prPoint + starPoint + forkPoint
+  }
 }
 
 object RepositoryRank extends IOApp.Simple {
